6cc9c33caffce990f749eb6e9e7c43e45542cf97
Anton Seleznev <yqks@icloud.com>
2024-09-09 17:05:43 +0700

    ESLint fixes

--- src/common/helper/runMigration.helper.ts
+++ src/common/helper/runMigration.helper.ts
@@ -1,20 +1,20 @@
-import { db } from '../plugin/kysely.plugin';
 import { promises as fs } from 'fs';
 import path from 'path';
-import {consoleColor} from "../constant/consoleColor.constant";
+import { consoleColor } from '../constant/consoleColor.constant';
+import { db } from '../plugin/kysely.plugin';
 
 async function runMigrations() {
-    const migrationsDir = path.join(__dirname, '../migrations');
-    const files = await fs.readdir(migrationsDir);
+  const migrationsDir = path.join(__dirname, '../migrations');
+  const files = await fs.readdir(migrationsDir);
 
-    for (const file of files) {
-        const migration = require(path.join(migrationsDir, file));
-        console.log(consoleColor.FG.GREEN, `[DB] Running migration: ${file}`);
-        await migration.up(db);
-    }
+  for (const file of files) {
+    const migration = require(path.join(migrationsDir, file));
+    console.log(consoleColor.FG.GREEN, `[DB] Running migration: ${file}`);
+    await migration.up(db);
+  }
 
-    console.log(consoleColor.FG.GREEN, '[DB] Migrations completed');
-    await db.destroy();
+  console.log(consoleColor.FG.GREEN, '[DB] Migrations completed');
+  await db.destroy();
 }
 
 runMigrations().catch(console.error);
